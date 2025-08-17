14436a8d9991fa27982a2c3e3b39afb04a7f0a68
chadmoyashi <kenro.oyama@gmail.com>
2025-08-17 15:13:51 +0900

    Initial commit

---
+++ styles.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for navbar links
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  
